1e8b738849759435107f23c29f39ab8716e5faa8
samposig <samposig@gmail.com>
2022-04-25 16:15:49 -0600

    working with colors

--- script.js
+++ script.js
@@ -1,51 +1,65 @@
-var currentDay = $("#currentDay");
-var schedule = $(".container");
-var timeBlock = $(".time-blocks");
-var hour = $(".hour");
-var scheduleText = $("textarea");
-var saveButtons = $(".saveBtn");
+$(document).ready(function () {
 
-$(start);
-function start() {
-  // get current day and display on top of page
-  $(".currentDay").text(moment().format("dddd, MMMM Do"));
+  //Setting up the date and appending to the HTML page
+  var today = moment().format('MMMM Do YYYY');
+  console.log(today)
+  $("#currentDay").append(today);
 
-  // color our time blocks and start interval to re-color every minute
-  colorTimeBlocks();
-  setInterval(colorTimeBlocks, 60000);
+  var now = parseInt(moment().format('HH'));
+  console.log(now)
 
-  // update time blocks with data in local storage
-  $(".time-blocks").each(function() {
-    var Id = $(this).attr("id");
-    // load saved data from local storage
-    $("#" + Id + " textarea").text(localStorage.getItem(moment().format("DDDYYYY") + Id));
-  });
+  var $9AM = $("#9AM");
+  var $10AM = $("#10AM");
+  var $11AM = $("#11AM");
+  var $12AM = $("#12AM");
+  var $1PM = $("#1PM");
+  var $2PM = $("#2PM");
+  var $3PM = $("#3PM");
+  var $4PM = $("#4PM");
+  var $5PM = $("#5PM");
 
-  // attach our handler for the save buttons
-  $(".saveBtn").on("click", handleSave);
-}
 
-function colorTimeBlocks() {
-  // for each time block
-  $(".time-blocks").each(function() {
-    var blockHour = parseInt($(this).attr("id").replace("hour-", ""));
-    var currentHour = parseInt(moment().format("H"));
-    // remove any class we may have added before
-    $(this).removeClass("past present future");
-    // color block based on past, present, future class
-    if (blockHour < currentHour) {
-      $(this).addClass("past");
-    } else if (blockHour > currentHour) {
-      $(this).addClass("future");
-    } else {
-      $(this).addClass("present");
-    }
-  });
-}
+  $("textarea").each(function () {
+      var name = parseInt($(this).attr("name"));
+      if (name < now) {
+          $(this).addClass("bg-gray");
+      }
+      if (name > now) {
+          $(this).addClass("bg-green")
+      }
 
-function handleSave(event) {
-  // get the id of our parent
-  var hourId = $(this).parent().attr("id");
-  // save data in textarea in local storage
-  localStorage.setItem(moment().format("DDDYYYY") + hourId, $("#" + hourId + " textarea").val());
-}
+      if (name === now) {
+          $(this).addClass("bg-red")
+      }
+
+
+
+  })
+
+  $("button").on("click", function () {
+
+      //setting items in the local storage
+      localStorage.setItem("9AM", ($9AM.val()))
+      localStorage.setItem("10AM", ($10AM.val()))
+      localStorage.setItem("11AM", ($11AM.val()))
+      localStorage.setItem("12AM", ($12AM.val()))
+      localStorage.setItem("13PM", ($1PM.val()))
+      localStorage.setItem("14PM", ($2PM.val()))
+      localStorage.setItem("15PM", ($3PM.val()))
+      localStorage.setItem("16PM", ($4PM.val()))
+      localStorage.setItem("17PM", ($5PM.val()))
+
+  })
+
+  //getting the content stored and sending to the screen. When page is refreshed content will stay
+  $("#9AM").append(localStorage.getItem("9AM"));
+  $("#10AM").append(localStorage.getItem("10AM"));
+  $("#11AM").append(localStorage.getItem("11AM"));
+  $("#12AM").append(localStorage.getItem("12AM"));
+  $("#1PM").append(localStorage.getItem("13PM"));
+  $("#2PM").append(localStorage.getItem("14PM"));
+  $("#3PM").append(localStorage.getItem("15PM"));
+  $("#4PM").append(localStorage.getItem("16PM"));
+  $("#5PM").append(localStorage.getItem("17PM"));
+
+})
